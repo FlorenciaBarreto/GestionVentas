454868bebef32a825775a2941ee800d93010bd7e
FlorenciaBarreto <florenciabarreto@hotmail.com>
2024-10-31 02:40:46 +0100

    Básicos en el service de cliente y comercial

--- services/clienteService.js
+++ services/clienteService.js
@@ -1,14 +1,66 @@
 const db = require('./db');
 const helper = require('../helper');
 
+//Get Full Clients list
+async function getClientsList() {
+  let sql ="SELECT * FROM cliente;";
+
+  console.debug("La consulta en SQL es: " + sql);
+  const rows = await db.query(sql);
+  const info = helper.emptyOrRows(rows);
+  return{info};
+}
 
 
+//Get Clientes por id
+async function getOneClient(id){
+  let sql = "SELECT * FROM cliente WHERE id="+id+";";
+  
+  console.debug("La consulta en SQL es: " + sql);
+  const rows = await db.query(sql);
+  const info = helper.emptyOrRows(rows);
+  return{info};
+}
 
 
+//Crear nuevo cliente
+async function createNewClient(cliente){
+  let sql = "INSERT INTO cliente (nombre, apellido1, apellido2, ciudad, categoria) VALUES (";
+  sql += "'" + cliente.nombre + "', '" + cliente.apellido1 + "', '" + cliente.apellido2 + "', '" + cliente.ciudad + "', '" + cliente.categoria + ");";
+      
+  console.debug("La consulta en SQL es: " + sql);
+  const rows = await db.query(sql);
+  const info = helper.emptyOrRows(rows);
+  return{info};
+}
+
+//Borrar Cliente
+async function deleteClient(id) {
+  let sql = "DELETE FROM cliente WHERE id='"+ id +"';"
+
+  console.debug("La consulta en SQL es: " + sql);
+  const rows = await db.query(sql);
+  const info = helper.emptyOrRows(rows);
+  return{info};
+}
+
+
+//Modificar Comercial
+async function modifyClient(cliente) {
+  let sql = "UPDATE cliente SET nombre='" + cliente.nombre + "', apellido1='" + cliente.apellido1 + "', apellido2='" + cliente.apellido2 + "', ciudad='" + cliente.ciudad + "', categoria='" + cliente.categoria + "' WHERE id=" + comercial.id + ";";
+
+  console.debug("La consulta en SQL es: " + sql);
+  const rows = await db.query(sql);
+  const info = helper.emptyOrRows(rows);
+  return{info};
+}
 
 
 //cambiar nombres de las funciones
 module.exports = {
-    
-   
+    getClientsList,
+    getOneClient,
+    createNewClient,
+    deleteClient,
+    modifyClient       
   }
